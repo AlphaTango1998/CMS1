bdebe1e340d5be7766cdc2d5c16e27e24b4703b5
Ashutosh1Adglobal360 <110081819+Ashutosh1Adglobal360@users.noreply.github.com>
2022-08-22 13:12:36 +0530

    Alpha1

--- clientside/src/component/Login.js
+++ clientside/src/component/Login.js
@@ -48,7 +48,7 @@ function Login() {
             <div className="col-lg-4"></div>
             <div className="col-lg-4">
                <label>Password</label>
-               <input type="text" 
+               <input type="password" 
                 name="password"
                 onChange={(e)=>onValueChange(e)}
                className="form-control" />
