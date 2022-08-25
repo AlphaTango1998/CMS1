ed08e8c2be27687dc2fe67acf6e85a8454f453a2
shubhamdhoot0303 <shubham.dhoot@adglobal333.com>
2022-08-25 10:22:32 +0530

    remove errors

--- clientside/src/component/Login_user.js
+++ clientside/src/component/Login_user.js
@@ -1,75 +1,93 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "./Navbar";
-import Sidebar from "./Sidebar";
-import { getAll } from "../service/api";
-import { useCookies } from "react-cookie";
+import React,{  useState,useEffect } from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import  { getAll } from '../service/api';
+import { useCookies } from 'react-cookie';
 function Login_user() {
-  const [userData, setUserData] = useState({ cod: "404" });
-  const [cookies, setCookie] = useCookies(["user"]);
-  let token_value = cookies.jwtoken;
-  useEffect(() => {
-    getAllUser();
-  }, []);
-  const getAllUser = async () => {
-    const user_data = await getAll(token_value);
-    setUserData(user_data);
+    const [userData, setUserData] = useState({"cod":"404"});
+    const [cookies, setCookie] = useCookies(['user']);
+    let token_value=cookies.jwtoken;
+       useEffect(()=>{
+        getAllUser();
+      },[]);
+    const getAllUser = async() =>{
+      
+       const user_data = await getAll( token_value);
+           setUserData(user_data);
 
-     console.log(user_data)
-    //console.log(user_data.data[0].fname)
-  };
+        // console.log(user_data)
+        //console.log(user_data.data[0].fname)
+        }
+
 
   return (
     <>
-      <div>
-        <div className="row">
-          <div className="col-lg-12">
-            <Navbar />{" "}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-3">
-            <Sidebar />{" "}
-          </div>
-          <div className="col-lg-9">
-            <h3 className="text-center">Login user Details </h3>
-            <div>
-             
-                <div className="card text-center">
-                  <div className="card-header">data not found</div>
-                </div>
-             
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th>*</th>
-                      <th>First Name</th>
-                      <th>Last Name</th>
-                      <th>Email</th>
-                      <th>Phone</th>
-                      <th>DOB</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    { userData.data.map((value, id) => (
-                      <tr key={id}>
-                        <td> *</td>
-                        <td> {value.fname} </td>
-                        <td> {value.lname} </td>
-                        <td> {value.email} </td>
-                        <td> {value.phone} </td>
-                        <td> {value.dob} </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-          
-            </div>
-          </div>
-        </div>
+    <div>
+    <div className="row">
+        <div className="col-lg-12"><Navbar /> </div>  
       </div>
+      <div className= "row">
+     <div className="col-lg-3"><Sidebar /> </div>
+     <div className="col-lg-9"> 
+          <h3 className="text-center">Login user Details </h3>
+        <div>
+    
+          { userData?.cod==='404' ? (
+                     
+                     <div className="card text-center">
+                     <div className="card-header">
+                          data not found
+                     </div>                
+                     </div>
+
+           ):(  
+            
+            <table className="table">
+           
+           <thead>
+             <tr>
+               <th>*</th>
+                <th>First Name</th>
+               <th >Last Name</th>
+               <th >Email</th>
+               <th >Phone</th>
+               <th >DOB</th>
+               </tr>
+           </thead>
+           
+
+           <tbody>
+               {
+          userData.data.map((value,id)=> 
+          <tr key={id}>  
+          <td > *</td>
+          <td > { value.fname } </td>
+          <td > { value.lname } </td>
+          <td > { value.email } </td>
+          <td > { value.phone } </td>
+          <td > { value.dob } </td>
+          </tr>
+          )
+        }
+               
+             
+           
+           </tbody>
+        </table>  
+             ) }
+
+        
+    
+          
+      </div>
+      </div>
+      </div>
+        
+            
+
+    </div>
     </>
-  );
+  )
 }
 
-export default Login_user;
+export default Login_user
