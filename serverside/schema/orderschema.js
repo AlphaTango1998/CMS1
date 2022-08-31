2bf3b11646f12b5332db85454599bcb086405cfb
shubhamdhoot0303 <shubham.dhoot@adglobal333.com>
2022-08-31 16:58:44 +0530

    addded

--- serverside/schema/orderschema.js
+++ serverside/schema/orderschema.js
@@ -1,15 +1,13 @@
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema({
-    username:String,
-    productname:String,
-    quantity:String,
-    price:String,
-    totalamount:Number,
-    phonenumber:String,
-    
+  username: String,
+  productname: String,
+  quantity: String,
+  price: String,
+  totalamount: Number,
+  phonenumber: String,
 });
 
-const orderdatas = mongoose.model("orderdatas",orderSchema);
+const orderdatas = mongoose.model("orderdatas", orderSchema);
 
 export default orderdatas;
