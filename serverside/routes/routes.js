a7d7e41c4ed471c1015adff63c52755f1de04718
Shubham Maheshwari <110012111+shubhamdhoot0303@users.noreply.github.com>
2022-08-31 16:50:09 +0530

    api added

--- serverside/routes/routes.js
+++ serverside/routes/routes.js
@@ -19,6 +19,8 @@ import {
   addAddress,
   addCategories,
   getCategories,
+  setUser_register,
+  getUser_login,
 } from "../controllers/cms_controller.js";
 
 import Authenticate from "../middleware/authenticate.js";
@@ -49,6 +51,10 @@ router.get("/address", Authenticate, getAddress);
 router.get("/AddressDetail/:id", Authenticate, getAddressDetail);
 router.put("/EditAddress/:id", Authenticate, EditAddress);
 router.delete("/address/:id", Authenticate, DeleteAddress);
+//website side link
+router.post("/user/register", setUser_register);
+router.post("/user/login", getUser_login);
+
 
 export default router;
 
